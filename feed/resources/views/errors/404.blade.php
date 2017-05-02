690ee5a254d1b9a925ddc2264f98060569278e65
cleytonLuis <luisphptec@gmail.com>
2017-05-02 15:19:37 -0300

    finalizando a página de erro404

--- feed/resources/views/errors/404.blade.php
+++ feed/resources/views/errors/404.blade.php
@@ -2,8 +2,8 @@
 @section('conteudo') 
 	<div id="page-wrapper">
             <div class="row">
-                <div class="col-lg-12">
-    				<h2 class="alert alert-danger">Oops... Ocorreu um erro durante a navegação. Ja estamos resolvendo!</h2>
+                <div class="col-lg-12 col-md-offset-2">
+    				<img src="{{asset('/images/error/erro_404.png')}}" alt="Erro página não encontrada">
     			</div>
     		</div>
    </div> 
